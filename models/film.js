aed2d086666249280c2276239dea342ac981a030
Camilo Cárdenas <camicardenasp@gmail.com>
2023-02-14 09:32:39 -0500

    Changed films model.

--- models/film.js
+++ models/film.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const FilmSchema = new mongoose.Schema({
     title: String,
+    year: Number,
+    time: Number,
+    language: String,
+    release: String,
+    country: String,
     completed: Boolean
 });
 
