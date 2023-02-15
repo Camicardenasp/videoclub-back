fb33e9b84be0dd1b8cfaaa959856a6c5e1b5e9a1
Gerald <aligerald.cac7@gmail.com>
2023-02-15 05:00:48 -0500

    Add documentation backend

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express"); //Import express
+const mongoose = require("mongoose"); //Import mongoose
+const cors = require("cors"); //Import cors
+require("dotenv").config(); //Import dotenv
 
-const PUERTO = 3030;
+const PUERTO = 3030; //Create PORT the server 
 const app = express();
 
 const todoRoutes = require("./routes/todoRoutes");
-const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true };
+const connectionOptions = { useUnifiedTopology: true, useNewUrlParser: true }; //Configure connection options
 
 app.use(express.json());
 app.use(cors());
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect("mongodb+srv://cami:94012203084@films.c98s84j.mongodb.net/movies?retryWrites=true&w=majority", connectionOptions)
-    .then(() => console.log("Connected successfully"))
-    .catch((err) => console.error(err));
+mongoose.connect("mongodb+srv://cami:94012203084@films.c98s84j.mongodb.net/movies?retryWrites=true&w=majority", connectionOptions) //URI database
+    .then(() => console.log("Connected successfully")) //If the connection succeeds, it shows a success message
+    .catch((err) => console.error(err)); //If the connection fails, it shows an error message
 
 app.use("/films", todoRoutes);
 
 app.listen(process.env.PORT||PUERTO, () => {
-    console.log("The server is listening on port " + PUERTO);
+    console.log("The server is listening on port " + PUERTO); //Starts the server and displays a message with the port created
 });
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,32 +1,33 @@
-const router = require("express").Router();
-const Film = require("../models/film");
+const router = require("express").Router(); //Create constant router, import express
+const Film = require("../models/film"); //Import database model 
 
-router.get("/", (req, res) => {
-    Film.find((err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
+router.get("/", (req, res) => {  //Request GET in route
+    Film.find((err, result) => { //Search all documents in the films collection
+        if(err) throw new Error(err); //Enable conditional on error
+        res.json(result); //Returns the results in JSON format.
     });
 });
 
-router.post("/", (req, res) => {
-    Film.create(req.body, (err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
+router.post("/", (req, res) => { //Request POST in route
+    Film.create(req.body, (err, result) => { //Create a new document in the collection
+        if(err) throw new Error(err); //Enable conditional on error
+        res.json(result); //Returns the results in JSON format.
     });
 });
 
-router.put("/:id", (req, res) => {
-    Film.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
-        if(err) throw new Error(err);
-        res.json(result);
+router.put("/:id", (req, res) => { //Request PUT in route
+        if(err) throw new Error(err); //Enable conditional on error
+    Film.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => { //Search an document by ID in the films collection
+        if (err) throw new Error(err); //Enable conditional on error
+        res.json(result); //Returns the results in JSON format.
     });
 });
 
-router.delete("/:id", (req, res) => {
-    Film.findOneAndRemove({ _id: req.params.id }, (err, result) => {
-        if(err) throw new Error(err);
+router.delete("/:id", (req, res) => { //Request DELETE in route
+    Film.findOneAndRemove({ _id: req.params.id }, (err, result) => {//Search an document remove in the films collection
+        if(err) throw new Error(err); //Enable conditional on error
         res.end();
     });
 });
 
-module.exports = router;
+module.exports = router;  //Export database router
